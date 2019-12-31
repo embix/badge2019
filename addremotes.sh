ceb4dc05a05dde775dccd3aa71b829ade29ca487
Michel Bretschneider <embix@gmx.net>
2019-12-31 20:34:31 +0100

    +interesting remotes

---
+++ addremotes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/bornhack/badge2019
+git remote add kristoff3r https://github.com/kristoff3r/badge2019
+git remote add nielsek https://github.com/nielsek/badge2019
+git remote add sebastianpc https://github.com/sebastianpc/badge2019
+git remote add radicalpet https://github.com/radicalpet/badge2019
